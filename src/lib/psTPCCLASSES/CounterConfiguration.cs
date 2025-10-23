c19ffc87bffa0b7a70077369c3ac73559e125f96
Gabriel Köhl <162951091+gabrielkoehl@users.noreply.github.com>
2025-10-23 12:39:01 +0200

    DEV: Class SetRemoteConnectionParameter

--- src/lib/psTPCCLASSES/CounterConfiguration.cs
+++ src/lib/psTPCCLASSES/CounterConfiguration.cs
@@ -9,97 +9,108 @@ namespace psTPCCLASSES;
 
 public class CounterConfiguration
 {
-    public string CounterID { get; set; }
-    public string CounterSetType { get; set; }
-    public string CounterInstance { get; set; }
-    public string CounterPath { get; set; }
-    public string Title { get; set; }
-    public string Type { get; set; }
-    public string Format { get; set; }
-    public int ConversionFactor { get; set; }
-    public int ConversionExponent { get; set; }
-    public string Unit { get; set; }
-    public Dictionary<string, object> GraphConfiguration { get; set; }
-    public record DataPoint(DateTime Timestamp, double Value);
-    public List<DataPoint> HistoricalData { get; set; }
-    public int ExecutionDuration { get; set; }
-    public Dictionary<int, string> ColorMap { get; set; }
-    public Dictionary<string, object> Statistics { get; set; }
-    public bool IsAvailable { get; set; }
-    public bool IsRemote { get; set; }
-    public string ComputerName { get; set; }
-    public PSCredential? Credential { get; set; }
-    public Dictionary<string, object> ParamRemote { get; set; }
-    public string LastError { get; set; }
-    public DateTime? LastUpdate { get; set; }
+     public string CounterID { get; set; }
+     public string CounterSetType { get; set; }
+     public string CounterInstance { get; set; }
+     public string CounterPath { get; set; }
+     public string Title { get; set; }
+     public string Type { get; set; }
+     public string Format { get; set; }
+     public int ConversionFactor { get; set; }
+     public int ConversionExponent { get; set; }
+     public string Unit { get; set; }
+     public Dictionary<string, object> GraphConfiguration { get; set; }
+     public record DataPoint(DateTime Timestamp, double Value);
+     public List<DataPoint> HistoricalData { get; set; }
+     public int ExecutionDuration { get; set; }
+     public Dictionary<int, string> ColorMap { get; set; }
+     public Dictionary<string, object> Statistics { get; set; }
+     public bool IsAvailable { get; set; }
+     public bool IsRemote { get; set; }
+     public string ComputerName { get; set; }
+     public PSCredential? Credential { get; set; }
+     public Dictionary<string, object> ParamRemote { get; set; }
+     public string LastError { get; set; }
+     public DateTime? LastUpdate { get; set; }
 
-    public CounterConfiguration(
-        string counterID,
-        string counterSetType,
-        string counterInstance,
-        string title,
-        string type,
-        string format,
-        string unit,
-        int conversionFactor,
-        int conversionExponent,
-        object colorMap,
-        object graphConfiguration,
-        bool isRemote,
-        string computerName,
-        PSCredential? credential)
-    {
-        CounterID             = counterID;
-        CounterSetType        = counterSetType;
-        CounterInstance       = counterInstance;
-        Title                 = title;
-        Type                  = type;
-        Format                = format;
-        Unit                  = unit;
-        ConversionFactor      = conversionFactor;
-        ConversionExponent    = conversionExponent;
-        HistoricalData        = new List<DataPoint>();
-        Statistics            = new Dictionary<string, object>();
-        IsAvailable           = false;
-        LastError             = "";
-        IsRemote              = isRemote;
-        ComputerName          = computerName;
-        Credential            = credential;
+     public CounterConfiguration(
+          string counterID,
+          string counterSetType,
+          string counterInstance,
+          string title,
+          string type,
+          string format,
+          string unit,
+          int conversionFactor,
+          int conversionExponent,
+          object colorMap,
+          object graphConfiguration,
+          bool isRemote,
+          string computerName,
+          PSCredential? credential)
+     {
+          CounterID             = counterID;
+          CounterSetType        = counterSetType;
+          CounterInstance       = counterInstance;
+          Title                 = title;
+          Type                  = type;
+          Format                = format;
+          Unit                  = unit;
+          ConversionFactor      = conversionFactor;
+          ConversionExponent    = conversionExponent;
+          HistoricalData        = new List<DataPoint>();
+          Statistics            = new Dictionary<string, object>();
+          IsAvailable           = false;
+          LastError             = "";
+          IsRemote              = isRemote;
+          ComputerName          = computerName;
+          Credential            = credential;
 
-        SetRemoteConnectionParameter();
+          SetRemoteConnectionParameter();
 
-        CounterPath           = GetCounterPath(counterID, counterSetType, counterInstance);
-        ColorMap              = SetColorMap(colorMap);
-        GraphConfiguration    = SetGraphConfig(graphConfiguration);
+          CounterPath           = GetCounterPath(counterID, counterSetType, counterInstance);
+          ColorMap              = SetColorMap(colorMap);
+          GraphConfiguration    = SetGraphConfig(graphConfiguration);
 
-        TestAvailability();
-    }
+          TestAvailability();
+     }
 
-    private void SetRemoteConnectionParameter()
-    {
+     private void SetRemoteConnectionParameter()
+     {
 
-    }
+          ParamRemote = new Dictionary<string, object>
+          {
+               { "ComputerName", ComputerName }
+          };
 
-    private string GetCounterPath(string counterID, string counterSetType, string counterInstance)
-    {
+          if (Credential is not null)
+          {
+               ParamRemote.Add("Credential", Credential);
+          }
 
-        return "";
-    }
+     }
 
-    private Dictionary<int, string> SetColorMap(object colorMap)
-    {
+     private string GetCounterPath(string counterID, string counterSetType, string counterInstance)
+     {
 
-        return new Dictionary<int, string>();
-    }
+          return "";
+     }
 
-    private Dictionary<string, object> SetGraphConfig(object graphConfiguration)
-    {
+     private Dictionary<int, string> SetColorMap(object colorMap)
+     {
 
-        return new Dictionary<string, object>();
-    }
+          return new Dictionary<int, string>();
+     }
 
-    private void TestAvailability()
-    {
+     private Dictionary<string, object> SetGraphConfig(object graphConfiguration)
+     {
 
-    }
+          return new Dictionary<string, object>();
+     }
+
+     private void TestAvailability()
+     {
+
+     }
+
 }
