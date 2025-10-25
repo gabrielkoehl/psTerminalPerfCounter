e490966b897b7cdbdbbe4ed2892d53eb317feb23
Gabriel Köhl <162951091+gabrielkoehl@users.noreply.github.com>
2025-10-25 20:10:03 +0200

    TESTING

--- run.ps1
+++ run.ps1
@@ -1,15 +1,15 @@
-#  dotnet build "src\lib\psTPCLIB.sln"
+  dotnet build "src\lib\psTPCLIB.sln"
 
-#   Copy-Item "src\lib\psTPCCLASSES\bin\Debug\net9.0\psTPCCLASSES.dll" "src\psTerminalPerCounter\psTerminalPerfCounter\Lib"
-#   Copy-Item "src\lib\psTPCCLASSES\bin\Debug\net9.0\psTPCCLASSES.deps.json" "src\psTerminalPerCounter\psTerminalPerfCounter\Lib"
+   Copy-Item "src\lib\psTPCCLASSES\bin\Debug\net9.0\psTPCCLASSES.dll" "src\psTerminalPerCounter\psTerminalPerfCounter\Lib"
+   Copy-Item "src\lib\psTPCCLASSES\bin\Debug\net9.0\psTPCCLASSES.deps.json" "src\psTerminalPerCounter\psTerminalPerfCounter\Lib"
 
- import-module -fullyqualifiedname ".\src\psTerminalPerCounter\psTerminalPerfCounter\psTerminalPerfCounter.psd1" -force
+#  import-module -fullyqualifiedname ".\src\psTerminalPerCounter\psTerminalPerfCounter\psTerminalPerfCounter.psd1" -force
 
 
 #start-tpcmonitor -RemoteServerConfig ".\_remoteconfigs\AD_SERVER_001.json" -showconsoletable
 
 #start-tpcmonitor -ComputerName DEV-NODE3 -ConfigName CPU
 
-start-tpcMonitor -ConfigName CPU
+#start-tpcMonitor -ConfigName CPU
 
 # BEI REMOTE GESAMTHEITLICHES INTERVALL SETZEN
