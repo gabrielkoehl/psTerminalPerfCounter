7cc4fdaa9c463e2403821b00cf91bd92b62f0b23
gabrielkoehl <162951091+gabrielkoehl@users.noreply.github.com>
2026-02-22 21:28:28 +0100

    DEV: Template filepath var

--- src/psTerminalPerfCounter/psTerminalPerfCounter/psTerminalPerfCounter.psm1
+++ src/psTerminalPerfCounter/psTerminalPerfCounter/psTerminalPerfCounter.psm1
@@ -6,6 +6,7 @@ $script:TPC_CONFIG_PATH_VAR             = "TPC_CONFIGPATH"
 $script:DEFAULT_CONFIG_PATH             = Join-Path $PSScriptRoot -ChildPath "Config"
 $script:JSON_SCHEMA_CONFIG_FILE         = Join-Path $script:DEFAULT_CONFIG_PATH -ChildPath "schema_config.json"
 $script:JSON_SCHEMA_ENVIRONMENT_FILE    = Join-Path $script:DEFAULT_CONFIG_PATH -ChildPath "schema_environment.json"
+$script:JSON_DEFAULT_TEMPLATE_FILE      = Join-Path $script:DEFAULT_CONFIG_PATH -ChildPath "default_template_values.json"
 
 # Loading Logger (Singleton)
 $script:logger = [psTPCCLASSES.PowerShellLogger]::Instance
